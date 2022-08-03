8391fef68024816036c362fdafed4fd834817307
Hanz <22578473+fivehanz@users.noreply.github.com>
2022-08-03 20:58:30 +0530

    chore: badge & js init

--- Dog.js
+++ Dog.js
@@ -1,1 +1,9 @@
 // Create the Dog class here
+
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+}
+
+export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Remember to import the data and Dog class!
+
+import Dog from "./Dog";
+import { dogs } from "./data";
